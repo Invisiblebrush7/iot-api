974d2723c67adf24cf7a6ef1d686207e394ef01f
alberto romano <aromanog1@gmail.com>
2022-11-17 18:06:33 -0600

    Testing

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,7 +7,7 @@ const { ensureAuth, ensureGuest } = require('../middlewares/auth');
 
 router.post('/sign_up', ensureGuest, UsersController.signUpPost);
 router.post('/login', ensureGuest, UsersController.login);
-router.get('/users', (req, res) => {
+router.get('/users', ensureAuth, (req, res) => {
 	return res.status(200).send('This is a pretty cool route');
 });
 router.post('/logout', ensureAuth, UsersController.logout);
